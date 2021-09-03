dc62a29e3dbda38984c0fbfe4a093afb22aa5088
dondo1437@gmail.com <dondo1437@gmail.com>
2021-09-03 15:15:52 +0300

    chapter 1 implemented

---
+++ src/main/java/com/es/phoneshop/model/product/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.es.phoneshop.model.product.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(){
+        super();
+    }
+}
